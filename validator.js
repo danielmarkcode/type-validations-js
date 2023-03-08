3076976309a355d4b3444ac0fbb5a2322529b63a
Daniel Mark <49250267+danielmarkcode@users.noreply.github.com>
2023-03-08 20:48:19 +0530

    Update validator.js

--- validator.js
+++ validator.js
@@ -1,40 +1,76 @@
-const validator = {
-  get(target, prop) {
-    if (!(prop in target)) {
-      throw new TypeError(`Property "${prop}" does not exist on object`);
-    }
-    return target[prop];
-  },
-  set(target, prop, value) {
-    if (typeof value !== target[prop].type) {
-      throw new TypeError(`Invalid type. Expected ${target[prop].type}, but got ${typeof value}`);
-    }
-    target[prop].value = value;
-    return true;
-  }
-};
+function typeCheck(object) {
+  const handler = {
+    get(target, prop) {
+      if (prop in target) {
+        return target[prop];
+      }
+      throw new Error(`Property "${prop}" does not exist.`);
+    },
+    set(target, prop, value) {
+      const type = prop.split(".").pop();
+      if (type === "string") {
+        if (typeof value !== "string") {
+          throw new Error(`Property "${prop}" must be a string.`);
+        }
+      } else if (type === "int") {
+        if (typeof value !== "number" || !Number.isInteger(value)) {
+          throw new Error(`Property "${prop}" must be an integer.`);
+        }
+      } else if (type === "float") {
+        if (typeof value !== "number" || Number.isNaN(value)) {
+          throw new Error(`Property "${prop}" must be a float.`);
+        }
+      } else if (type === "number") {
+        if (typeof value !== "number" || Number.isNaN(value)) {
+          throw new Error(`Property "${prop}" must be a number.`);
+        }
+      } else if (type === "bool") {
+        if (typeof value !== "boolean") {
+          throw new Error(`Property "${prop}" must be a boolean.`);
+        }
+      }
 
-function validateObject(obj, schema) {
-  const proxy = new Proxy(schema, validator);
-  for (let prop in obj) {
-    proxy[prop] = obj[prop];
+      if (!target.hasOwnProperty(prop) && !prop.endsWith(".any")) {
+        // Throw an error if creating a property with an invalid type
+        const validTypes = ["string", "int", "float", "number", "bool"];
+        if (!validTypes.includes(type)) {
+          throw new Error(`Property "${prop}" must have a valid type.`);
+        }
+      }
+
+      target[prop] = value;
+      return true;
+    },
+  };
+
+  // Validate initial values
+  for (const prop in object) {
+    const type = prop.split(".").pop();
+    if (type === "string") {
+      if (typeof object[prop] !== "string") {
+        throw new Error(`Property "${prop}" must be a string.`);
+      }
+    } else if (type === "int") {
+      if (typeof object[prop] !== "number" || !Number.isInteger(object[prop])) {
+        throw new Error(`Property "${prop}" must be an integer.`);
+      }
+    } else if (type === "float") {
+      if (typeof object[prop] !== "number" || Number.isNaN(object[prop])) {
+        throw new Error(`Property "${prop}" must be a float.`);
+      }
+    } else if (type === "number") {
+      if (typeof object[prop] !== "number" || Number.isNaN(object[prop])) {
+        throw new Error(`Property "${prop}" must be a number.`);
+      }
+    } else if (type === "bool") {
+      if (typeof object[prop] !== "boolean") {
+        throw new Error(`Property "${prop}" must be a boolean.`);
+      }
+    } else if (object[prop] !== null && typeof object[prop] === "object") {
+      // recursively validate complex objects
+      typeCheck(object[prop]);
+    }
   }
-  return proxy;
+
+  return new Proxy(object, handler);
 }
-
-const schema = {
-  name: { type: 'string', value: '' },
-  age: { type: 'number', value: 0 },
-  isAdmin: { type: 'boolean', value: false }
-};
-
-const obj = { name: 'John', age: 30, isAdmin: true };
-const validatedObj = validateObject(obj, schema);
-
-console.log(validatedObj.name); // 'John'
-console.log(validatedObj.age); // 30
-console.log(validatedObj.isAdmin); // true
-
-validatedObj.age = '30'; // throws TypeError: Invalid type. Expected number, but got string
-
-validatedObj.foo = 'bar'; // throws TypeError: Property "foo" does not exist on object
